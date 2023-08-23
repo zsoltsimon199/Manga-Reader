2d1e220716af086e2d2216ace17e48050d766c1f
zsoltika199 <zsoltsimon199@gmail.com>
2023-08-23 23:24:52 +0200

    move tyoe

--- app/src/main/java/com/zs/mangareader/core/presentation/reader/ui/theme/Type.kt
+++ app/src/main/java/com/zs/mangareader/core/presentation/reader/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.zs.mangareader.core.presentation.ui.theme
+package com.zs.mangareader.core.presentation.reader.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
