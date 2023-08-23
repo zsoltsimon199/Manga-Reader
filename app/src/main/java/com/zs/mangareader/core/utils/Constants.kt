3998489168e056bc19f6801f30668c6d5745c4e0
zsoltika199 <zsoltsimon199@gmail.com>
2023-08-23 23:26:03 +0200

    add constants, routes for nav

---
+++ app/src/main/java/com/zs/mangareader/core/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.zs.mangareader.core.utils
+
+object Constants {
+
+    const val URL = ""
+    const val DATABASE_NAME = ""
+    const val THEME_DATASTORE = "theme_datastore"
+    const val DATABASE_VERSION = 1
+
+}
---
+++ app/src/main/java/com/zs/mangareader/core/utils/Routes.kt
@@ -0,0 +1,11 @@
+package com.zs.mangareader.core.utils
+
+object Routes {
+
+    const val LIBRARY = "LIBRARY"
+    const val UPDATES = "UPDATES"
+    const val HISTORY = "HISTORY"
+    const val BROWSE = "BROWSE"
+    const val MORE = "MORE"
+
+}
