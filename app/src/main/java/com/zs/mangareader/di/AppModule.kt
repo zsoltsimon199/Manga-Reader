f7cd6fd81ff32866c77abd5e4d821576fff059ed
zsoltika199 <zsoltsimon199@gmail.com>
2023-08-24 19:19:50 +0200

    add di for api and usecases

--- app/src/main/java/com/zs/mangareader/di/AppModule.kt
+++ app/src/main/java/com/zs/mangareader/di/AppModule.kt
@@ -1,17 +1,41 @@
 package com.zs.mangareader.di
 
 import android.content.Context
+import com.zs.mangareader.core.data.remote.MangaApi
 import com.zs.mangareader.core.domain.helpers.ThemeDataStore
+import com.zs.mangareader.core.domain.repositories.MangaRepository
+import com.zs.mangareader.core.utils.Constants
+import com.zs.mangareader.featureBrowse.domain.useCase.GetMangasUseCase
+import com.zs.mangareader.featureBrowse.domain.useCase.MangaListUseCases
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofitApi(): MangaApi {
+        return Retrofit.Builder().baseUrl(Constants.URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MangaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMangaListUseCases(repository: MangaRepository): MangaListUseCases {
+        return MangaListUseCases(
+            getMangasUseCase = GetMangasUseCase(repository = repository),
+        )
+    }
 
     @Provides
     @Singleton
